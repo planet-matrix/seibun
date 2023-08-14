2c0a370ee3e841d8bab0317f51b7b19dcb1d5cee
Stephen Zhou <hi@hyoban.cc>
2023-08-14 20:52:27 +0800

    chore: add prettier plugin

--- packages/prettier-config/config.js
+++ packages/prettier-config/config.js
@@ -1,7 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
   semi: false,
-  plugins: ["prettier-plugin-astro"],
+  plugins: [
+    require.resolve("prettier-package-json"),
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-astro"),
+  ],
   overrides: [
     {
       files: "pnpm-lock.yaml",
@@ -16,4 +20,19 @@ module.exports = {
       },
     },
   ],
+  importOrderParserPlugins: [
+    "classProperties",
+    "decorators-legacy",
+    "typescript",
+    "jsx",
+  ],
+  importOrder: [
+    "<THIRD_PARTY_MODULES>",
+    "^@(.*)/(.*)$",
+    "^~/(.*)$",
+    "^@/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 }
