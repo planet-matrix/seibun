7c531e331f7960614606cdab49423f94c178347d
Stephen Zhou <hi@hyoban.cc>
2023-08-28 11:43:28 +0800

    feat: init docs

---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,16 @@
+import { DocsThemeConfig } from "nextra-theme-docs"
+import React from "react"
+
+const config: DocsThemeConfig = {
+  logo: <span>Hyoban's config</span>,
+  project: {
+    link: "https://github.com/hyoban/config",
+  },
+  docsRepositoryBase: "https://github.com/hyoban/config/tree/main/apps/docs",
+  footer: {
+    text: null,
+    component: null,
+  },
+}
+
+export default config
