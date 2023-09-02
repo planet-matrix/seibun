fc3c3182c459b5e1da1c6532b7c41745d5719c41
Stephen Zhou <hi@hyoban.cc>
2023-09-02 13:19:09 +0800

    docs: add button demo

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,9 +1,20 @@
-import { AppearanceSwitch } from "@hyoban/components"
+import { AppearanceSwitch, Button } from "@hyoban/components"
 
 export default function App() {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <AppearanceSwitch />
+      <main className="prose prose-neutral dark:prose-invert">
+        <h1>Button</h1>
+        <div className="flex items-center gap-x-4">
+          <Button>Default</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="outline">Outline</Button>
+          <Button variant="destructive">Destructive</Button>
+          <Button variant="link">Link</Button>
+          <Button variant="ghost">Ghost</Button>
+        </div>
+      </main>
+      <AppearanceSwitch className="mt-10" />
     </div>
   )
 }
