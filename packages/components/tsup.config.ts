f0a4f64c64785797b2f958f4aeef379d47e410d0
Stephen Zhou <hi@hyoban.cc>
2023-09-18 19:36:41 +0800

    fix(components): do not target to es2018

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -2,13 +2,9 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   entry: ["src/index.tsx", "src/button/index.tsx", "src/input/index.tsx"],
-  minify: false,
-  target: "es2018",
   external: ["react"],
-  sourcemap: false,
   dts: true,
   format: ["esm", "cjs"],
-  injectStyle: false,
   esbuildOptions(options) {
     options.banner = {
       js: '"use client"',
