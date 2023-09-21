850d2b7e7cc072d197dd9813a1808dbc041506c2
Stephen Zhou <hi@hyoban.cc>
2023-09-21 20:59:22 +0800

    fix(components): forward input ref

--- packages/components/src/input/index.tsx
+++ packages/components/src/input/index.tsx
@@ -7,11 +7,10 @@ export interface InputProps
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, iconClassName, ...props }, ref) => {
+  ({ className, iconClassName, ...props }, ref) => {
     if (!iconClassName) {
       return (
         <input
-          type={type}
           className={cn(
             "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             className,
@@ -23,7 +22,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     } else {
       return (
         <div className="relative w-full">
-          <Input {...props} className={cn("pl-8", className)} />
+          <Input className={cn("pl-8", className)} ref={ref} {...props} />
           <span
             className={cn(
               "text-[1.25rem] text-foreground absolute inset-0 top-1/2 -translate-y-1/2 left-2",
