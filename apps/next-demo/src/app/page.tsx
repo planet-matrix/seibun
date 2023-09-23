a55ad5aef4e874c2edb56cb5d41a4118dfea5638
Stephen Zhou <hi@hyoban.cc>
2023-09-23 23:54:25 +0800

    chore: try react-tweet

--- apps/next-demo/src/app/page.tsx
+++ apps/next-demo/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Tweet } from "react-tweet"
 import { AppearanceSwitch } from "@hyoban/components"
 import { cn } from "@hyoban/utils"
 
@@ -8,6 +9,7 @@ export default function Home() {
         "flex min-h-screen flex-col items-center justify-between p-24",
       )}
     >
+      <Tweet id="1705579477068325180" />
       <AppearanceSwitch enableTransition />
     </main>
   )
