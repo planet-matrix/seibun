47620a4e049d1bfcaede3f0da3dd03c140adf67a
Stephen Zhou <hi@hyoban.cc>
2023-10-08 21:57:09 +0800

    feat(components): select

--- apps/vite-demo/src/App.tsx
+++ apps/vite-demo/src/App.tsx
@@ -5,7 +5,34 @@ import {
   Input,
   Separator,
   TailwindIndicator,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "@hyoban/components"
+
+export function SelectDemo() {
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
 
 export function CheckboxDemo() {
   return (
@@ -45,6 +72,10 @@ export default function App() {
         </div>
         <h2>Separator</h2>
         <Separator orientation="horizontal" />
+        <h2>Select</h2>
+        <div>
+          <SelectDemo />
+        </div>
       </main>
       <AppearanceSwitch className="mt-10" enableTransition />
       <TailwindIndicator />
